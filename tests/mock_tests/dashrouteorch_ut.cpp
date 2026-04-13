d603e9a6e42b299299e699693411c54a3182e383
Charles Tsai <chartsai@nvidia.com>
2026-04-13 20:38:53 +0300

    Add UT

--- tests/mock_tests/dashrouteorch_ut.cpp
+++ tests/mock_tests/dashrouteorch_ut.cpp
@@ -10,9 +10,11 @@
 #include "mock_dash_orch_test.h"
 #include "dash_api/appliance.pb.h"
 #include "dash_api/route_type.pb.h"
+#include "dash_api/route.pb.h"
 #include "dash_api/eni.pb.h"
 #include "dash_api/qos.pb.h"
 #include "dash_api/eni_route.pb.h"
+#include "crmorch.h"
 
 EXTERN_MOCK_FNS
 namespace dashrouteorch_test
@@ -20,8 +22,18 @@ namespace dashrouteorch_test
     DEFINE_SAI_API_MOCK(dash_outbound_routing, outbound_routing);
     using namespace mock_orch_test;
     using ::testing::InSequence;
+    using ::testing::Return;
+    using ::testing::DoAll;
+    using ::testing::SetArrayArgument;
+
     class DashRouteOrchTest : public MockDashOrchTest
     {
+    protected:
+        int GetCrmUsedCount(CrmResourceType type)
+        {
+            return gCrmOrch->m_resourcesMap.at(type).countersMap["STATS"].usedCounter;
+        }
+
         void PostSetUp()
         {
             CreateApplianceEntry();
@@ -53,5 +65,25 @@ namespace dashrouteorch_test
         
         AddTunnel();
         AddOutboundRoutingEntry();
+    }
+
+    TEST_F(DashRouteOrchTest, RemoveNonexistOutboundRoutingDoesNotDecrementCrm)
+    {
+        AddOutboundRoutingGroup();
+        AddTunnel();
+
+        int baselineUsed = GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_ROUTING);
+        AddOutboundRoutingEntry();
+        EXPECT_EQ(GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_ROUTING), baselineUsed + 1);
+
+        RemoveOutboundRoutingEntry();
+        EXPECT_EQ(GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_ROUTING), baselineUsed);
+
+        // Remove non-existent outbound routing entry should return SAI_STATUS_ITEM_NOT_FOUND and not decrement the CRM used count
+        std::vector<sai_status_t> exp_status = {SAI_STATUS_ITEM_NOT_FOUND};
+        EXPECT_CALL(*mock_sai_dash_outbound_routing_api, remove_outbound_routing_entries)
+            .Times(1).WillOnce(DoAll(SetArrayArgument<3>(exp_status.begin(), exp_status.end()), Return(SAI_STATUS_SUCCESS)));
+        RemoveOutboundRoutingEntry();
+        EXPECT_EQ(GetCrmUsedCount(CrmResourceType::CRM_DASH_IPV4_OUTBOUND_ROUTING), baselineUsed);
     }
 }
--- tests/mock_tests/mock_dash_orch_test.h
+++ tests/mock_tests/mock_dash_orch_test.h
@@ -30,6 +30,7 @@ namespace mock_orch_test
             void RemoveVnetMap();
             void AddOutboundRoutingGroup();
             void AddOutboundRoutingEntry(bool expect_empty = true);
+            void RemoveOutboundRoutingEntry(bool expect_empty = true);
             void AddTunnel();
             void AddVnetMapPL(bool expect_empty = true);
             void RemoveVnetMapPL(bool expect_empty = true);
